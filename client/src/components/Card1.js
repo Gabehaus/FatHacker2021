ab28dcb7845d4bd8004e9a0740bf2243029eb06d
Gabehaus <Gabehaus@gmail.com>
2020-11-25 11:12:06 -0800

    learn more changed to LINK in card component

--- client/src/components/Card1.js
+++ client/src/components/Card1.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import { Button } from "reactstrap";
-import Image from "react-bootstrap/Image";
+import React, { Component } from "react"
+import { Button } from "reactstrap"
+import Image from "react-bootstrap/Image"
 
 export default class Card1 extends Component {
   render() {
     return (
-      <div className="fullCardBox1">
-        <div className="cardTitleBox">
-          <div className="profileImageBox">
+      <div className='fullCardBox1'>
+        <div className='cardTitleBox'>
+          <div className='profileImageBox'>
             {" "}
             <Image src={this.props.image} style={{ width: "100%" }} />
           </div>
         </div>
 
-        <div className="jobBox1">
-          <div className="name">{this.props.name}</div>
+        <div className='jobBox1'>
+          <div className='name'>{this.props.name}</div>
         </div>
-        <div className="textBox1">
-          <div className="actualText">{this.props.summary}</div>
+        <div className='textBox1'>
+          <div className='actualText'>{this.props.summary}</div>
           <Button
             href={this.props.buttonSource}
-            size="md"
-            className="cardButton"
+            size='md'
+            className='cardButton'
           >
             {" "}
-            LEARN MORE
+            LINK
           </Button>
         </div>
       </div>
-    );
+    )
   }
 }
